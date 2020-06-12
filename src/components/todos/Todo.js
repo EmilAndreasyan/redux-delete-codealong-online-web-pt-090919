7021ab63e5141f4a411ea8692a4a38745a7a98c7
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 05:16:02 +0000

    Automatically backed up by Learn

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -4,7 +4,7 @@ const Todo = props => {
   return (
     <div>
     <span>{props.text}</span>
-    <button onClick={this.props.delete}>DELETE<button/>
+    <button onClick={() => props.delete(props.text)}>DELETE<button/>
     </div>
     )
 }
